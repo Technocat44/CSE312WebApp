53cdb8dcbbc8cf590fc5c438427aab33340ed131
jamesaqu <jamesaqu@buffalo.edu>
2022-02-24 23:06:39 -0500

    Added a message for the grader about Postman issues

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # CSE312WebApp
-
+TODO: If you are grading this be aware that Postman is not fully functional with my build. The reason is unknown at the time of this 
+TODO: submission. I have gone to OH's multiple times and spoke with Jesse so if you have any questions please refer to him.
+TODO: I can still use Postman to send the request but Postman does not always get a response. The changes are reflected though
+TODO: all you need to do is check from the localhost port and refresh after a POST or DELETE for example. thank you
 This is the start of my CSE 312 Web App repo. 
--- server/server.py
+++ server/server.py
@@ -10,7 +10,12 @@ import usersResponse
 # test.sayHello()
 gh = osHandlers.addForwardSlash("\http\gggg\www\.com")
 print(gh)
+
 """
+TODO: If you are grading this be aware that Postman is not fully functional with my build. The reason is unknown at the time of this 
+TODO: submission. I have gone to OH's multiple times and spoke with Jesse so if you have any questions please refer to him.
+TODO: I can still use Postman to send the request but Postman does not always get a response. The changes are reflected though
+TODO: all you need to do is check from the localhost port and refresh after a POST or DELETE for example. thank you
 The head of request will consist of three parts:
     1. A request line 
         <Request_Method> <Path> <HTTP_version>
@@ -159,7 +164,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             respond = buildResponse.build200Response()
             print("Hello response: ", respond)
             self.request.sendall(respond)
-            
+
         if (request_method == "GET" and request_path == "/hi"):
             respond = buildResponse.build301Response()
             self.request.sendall(respond)
